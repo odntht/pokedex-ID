ce30428a016bc8599d53f74cc8c1ea5bfbe0011c
odntht <odntht@gmail.com>
2021-01-19 22:47:37 -0300

    adicionado componente da barra de busca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 import { PokemonItemComponent } from './components/pokemon-item/pokemon-item.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { MatSliderModule } from '@angular/material/slider';
+
 
 
 
@@ -16,13 +19,16 @@ import { MatSliderModule } from '@angular/material/slider';
     AppComponent,
     PokemonItemComponent,
     PokemonListComponent,
-    SearchBarComponent
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatCardModule,
+    MatAutocompleteModule,
+    MatGridListModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  myControl = new FormControl();
+  //Aqui virá a lista com os nomes dos pokemons
+  options: string[] = ['One', 'Two', 'Three','quatro'];
+  filteredOptions?: Observable<string[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
   }
 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
 }
