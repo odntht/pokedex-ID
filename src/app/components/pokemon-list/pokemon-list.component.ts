879523358039087ef9842c12b770a54fe072d03f
odntht <odntht@gmail.com>
2021-01-21 20:58:51 -0300

    add lista de pokemons

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent  {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+   
   }
+
 
 }
