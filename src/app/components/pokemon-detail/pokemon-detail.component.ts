5f31f782aba4775d15318c1f6650ab533920bece
odntht <odntht@gmail.com>
2021-01-21 21:00:06 -0300

    Add detalhes

---
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Inp } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-detail',
+  templateUrl: './pokemon-detail.component.html',
+  styles: [
+  ]
+})
+export class PokemonDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
